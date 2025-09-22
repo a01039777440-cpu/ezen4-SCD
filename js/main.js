f5b9d0ee2b4f505af8d7365b1ccebd142b449e3b
은희 <withjesus211@gmail.com>
2025-09-22 18:43:39 +0900

    슬라이드플러그인

--- js/main.js
+++ js/main.js
@@ -2,9 +2,22 @@
 
 // 메인 퀵메뉴 클래스 바꾸기
 let quickBtns = document.querySelectorAll('.aside-menu>li');
-quickBtns.forEach(qBtn =>{
-    qBtn.addEventListener('click',()=>{
-        quickBtns.forEach(q=>q.classList.remove('active'))        
+quickBtns.forEach(qBtn => {
+    qBtn.addEventListener('click', () => {
+        quickBtns.forEach(q => q.classList.remove('active'))
         qBtn.classList.add('active')
     })
 })
+
+var swiper = new Swiper(".main-slider", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
