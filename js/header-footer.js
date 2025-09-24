df4494926ec0c9219d7e2c9ae614c8a2f4326dc4
은희 <withjesus211@gmail.com>
2025-09-24 17:37:47 +0900

    메인비디오재생버튼 추가

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,25 +1,31 @@
 // header 불러오기
 fetch('header.html')
-.then(res => res.text())
-.then(data =>{
-    document.querySelector('#header').innerHTML=data;
-    
-    // 스크립트 
-    // GNB 언어 서브 펼침 이벤트
-    lang = document.querySelector('.lang>a');
-    langList = document.querySelector('.lang-list');
-    lang.addEventListener('click', () => {
-        lang.classList.toggle('active');
-        
+    .then(res => res.text())
+    .then(data => {
+        document.querySelector('#header').innerHTML = data;
+
+        // 스크립트 
+        // GNB 언어 서브 펼침 이벤트
+        lang = document.querySelector('.lang>a');
+        // langActive = document.querySelector('.lang>a.active');
+        langList = document.querySelector('.lang-list');
+        lang.addEventListener('click', (e) => {
+            lang.classList.add('active');
+            langList.style.display = "block"
+            // langActive.style.border="1px solid black"
+        })
+        // langActive.addEventListener('click', (e) => {
+        //     lang.classList.remove('active');
+        //     langList.style.display = "none"
+        // })
     })
-})
 
 // footer 불러오기
 fetch('footer.html')
-.then(res =>res.text())
-.then(data=>{
-    document.querySelector('#footer').innerHTML=data;
-    
-    // 스크립트 
-    
-})
+    .then(res => res.text())
+    .then(data => {
+        document.querySelector('#footer').innerHTML = data;
+
+        // 스크립트 
+
+    })
--- js/main.js
+++ js/main.js
@@ -9,8 +9,14 @@ quickBtns.forEach(qBtn => {
     })
 })
 
+// 최상단 슬라이더
 var swiper = new Swiper(".main-slider", {
+    // effect:"fade",
     loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    speed: 1000,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -21,3 +27,31 @@ var swiper = new Swiper(".main-slider", {
         clickable: true,
     },
 });
+
+// 프로모션 비디오 컨트롤
+let mainVideo = document.querySelector(".promotion-movie video");
+let playPause = document.querySelector(".playPause");
+let stopBtn = document.querySelector(".stop-btn");
+let prevBtn = document.querySelector(".prev-btn");
+let nextBtn = document.querySelector(".next-btn");
+
+playPause.addEventListener("click", function () {
+    if (mainVideo.paused) {
+        mainVideo.play(),
+            this.classList.add("active");
+    } else {
+        mainVideo.pause(),
+            this.classList.remove("active")
+    }
+})
+stopBtn.addEventListener("click", function () {
+    mainVideo.pause();
+    mainVideo.currentTime = 0;
+    playPause.classList.remove("active");
+})
+prevBtn.addEventListener("click", function () {
+    mainVideo.currentTime -= 2;
+})
+nextBtn.addEventListener("click", function () {
+    mainVideo.currentTime += 2;
+})
